efa2a5fb7cd0244a40b83138d2453c2174b62bb8
josiahwebb <jj.webb520@gmail.com>
2022-03-04 16:07:52 -0700

    gives blobs random colors, creates data var for blob info, creates food eaten array to store whats been eaten

--- javascript/client/main.js
+++ javascript/client/main.js
@@ -1,8 +1,15 @@
 let blob;
 let food = [];
 let zoom = 1;
+//array to be sent to server to show what's been eaten
+let foodEaten = [];
 
-
+//player blob data
+let data = {
+    x: blob.x,
+    y: blob.y,
+    r: blob.r
+}
 
 // Setup plan with server/client code
 // Food will be instantiated by the server - an array 
@@ -12,11 +19,15 @@ function setup() {
     createCanvas(1000, 1000);
     blob = new Blob(0, 0, 64);
     for (let i = 0; i < 100; i++) {
+        //positions will need to be fed from server
         let x = random(-width,width)
         let y = random(-height,height)
+        //this can prolly be kept to show food from server
         food[i] = new Blob(x, y, 15);
     }
 }
+
+
 
 function draw() {
     background(0);
@@ -32,6 +43,8 @@ function draw() {
     for (let i = food.length-1; i >= 0; i--) {
         food[i].show();
         if (blob.eats(food[i])) {
+            //pushes food eaten to array that can be sent to server
+            foodEaten.push(i)
             food.splice(i, 1)
 
         }
@@ -45,7 +58,6 @@ function Blob(x, y, r) {
 
     this.update = function() {
         let newVelocity = createVector(mouseX-width/2, mouseY-height/2);
-        // velocity.sub(this.pos);
         newVelocity.setMag(3);
         this.velocity.lerp(newVelocity, .3)
         this.pos.add(this.velocity);
@@ -56,16 +68,21 @@ function Blob(x, y, r) {
         if (d < this.r + foodItem.r) {
             let sum = PI * this.r * this.r + PI * foodItem.r * foodItem.r
             this.r = sqrt(sum / PI)
-            // this.r += foodItem.r*.2
             return true
         }
         return false;
     }
 
+    let red = randomHex()
+    let green = randomHex()
+    let blue = randomHex()
     this.show = function () {
-        fill(255)
+        fill(red, green, blue)
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
     }
 }
 
 
+function randomHex() {
+    return Math.floor(Math.random() * 256)
+}
