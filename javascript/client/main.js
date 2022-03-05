b34c2795438695a32d487acabadb1886e485b154
Peter Myers <peter@pcmyers.com>
2022-03-05 16:36:33 -0500

    Removed extra tick notification and resolved conflict

--- javascript/client/main.js
+++ javascript/client/main.js
@@ -1,3 +1,5 @@
+const { callbackify } = require("util");
+
 let blob;
 let food = [];
 let zoom = 1;
@@ -48,6 +50,11 @@ function setup(x_pos, y_pos) {
 
 
 }
+
+io.on("GameUpdate", new Emitter.Listener() {
+    console.log("game update loop"),
+    callback();
+});
 
 joinButton.addEventListener('click', () => {
     io.emit('PlayerJoinRequest');
@@ -120,7 +127,6 @@ function Blob(x, y, r) {
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
     }
 }
-
 
 function randomHex() {
     return Math.floor(Math.random() * 256)
--- javascript/server/main.js
+++ javascript/server/main.js
@@ -54,7 +54,6 @@ function game_loop() {
   }
 
   io.emit("GameUpdate", contents);
-  //console.log('Server tick.');
 }
 
 setInterval(game_loop, (1000/tickrate))
