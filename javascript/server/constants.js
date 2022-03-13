44c7c0ec9bc41cbc9eb020dc4c744a66cf8fbf07
Kibouweb3 <100516482+Kibouweb3@users.noreply.github.com>
2022-03-13 05:13:11 +0300

    Removed constants.js

--- javascript/server/constants.js
+++
@@ -1,4 +0,0 @@
-const RPC_URL  = "www.example.com"
-const WALLET_SEED  = "dove lumber quote board young robust kit invite plastic regular skull history"
-const CONTRACT_ADDRESS  = "www.example.com"
-const WALLET_ADDRESS  = "www.example.com"
