f7d195d6f376ba9a778302e39595bccd05b30bef
josiahwebb <jj.webb520@gmail.com>
2022-03-23 13:54:17 -0600

    minimizes draw function

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ const showAccount = document.querySelector('#show-account')
 let signerNonce;
 let isAuthenticated = true; //TODO: must be false in final
 
-  
+if (typeof window.ethereum !== 'undefined') {
+    connectBtn.style.display = 'flex'
+    dlBtn.style.display = 'none'
+} else {
+    connectBtn.style.display = 'none'
+    dlBtn.style.display = 'flex'
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 };
@@ -16,31 +23,38 @@ function setup() {
 // Main Game Loop
 function draw() {
     background(0);
-
     if (currentGame.startGame && isAuthenticated) {
         if (currentGame.player() == undefined) {return};
-        let localModel = currentGame.player().model;
+            adjustViewport()
+            drawFood()
+            drawPlayers()
+    }
+}
 
-        translate(width/2, height/2)
-        let newZoom = 64 / localModel.r;
-        let zoom = 1;
-        zoom = lerp(zoom, newZoom, .1)
-        scale(zoom)
-        translate(-localModel.pos.x, -localModel.pos.y)
-        localModel.control();
+function adjustViewport() {
+    let localModel = currentGame.player().model;
+    translate(width/2, height/2)
+    let newZoom = 64 / localModel.r;
+    let zoom = 1;
+    zoom = lerp(zoom, newZoom, .1)
+    scale(zoom)
+    translate(-localModel.pos.x, -localModel.pos.y)
+    localModel.control();
+}
 
-        //iterate through the food array to get the food
-        for (let i = currentGame.food.length-1; i >= 0; i--) {
-            currentGame.food[i].show();
-        }
+function drawFood() {
+    for (let i = currentGame.food.length-1; i >= 0; i--) {
+        currentGame.food[i].show();
+    }
+}
 
-        for (let i = 0; i < currentGame.players.length; i++) {
-            if (currentGame.players[i] == undefined) {continue};
-                let blob = currentGame.players[i].model;
-                blob.show();
-                blob.update();
-                blob.constrain();
-        }
+function drawPlayers() {
+    for (let i = 0; i < currentGame.players.length; i++) {
+        if (currentGame.players[i] == undefined) {continue};
+            let blob = currentGame.players[i].model;
+            blob.show();
+            blob.update();
+            blob.constrain();
     }
 }
 
@@ -49,20 +63,10 @@ function Game() {
     this.playerID = undefined,
     this.startGame = true; //TODO: must be false in final
     this.players = [],
-
     this.player = () => this.players[this.playerID]
-
 }
 
 const currentGame = new Game()
-
-if (typeof window.ethereum !== 'undefined') {
-    connectBtn.style.display = 'flex'
-    dlBtn.style.display = 'none'
-} else {
-    connectBtn.style.display = 'none'
-    dlBtn.style.display = 'flex'
-}
 
 socket.on('AuthNonce', (nonce) => {
     signerNonce = nonce
@@ -90,8 +94,67 @@ async function getAccount() {
     }
 }
 
-//TODO: DELETE THIS
-joinButton.style.display = 'flex'
+joinButton.style.display = 'flex' //TODO: DELETE THIS
+
+function Blob(x, y, r) {
+    this.pos = createVector(x, y);
+    this.r = r;
+    this.velocity = createVector(0,0)
+
+    this.control = function() {
+        let newVelocity = createVector(mouseX-width/2, mouseY-height/2);
+        newVelocity.setMag(3);
+        this.velocity.lerp(newVelocity, .3);
+    }
+
+    this.update = function() {
+        this.pos.add(this.velocity);
+    }
+
+    this.eats = function (foodItem) {
+        let d = p5.Vector.dist(this.pos, foodItem.pos)
+        if (d < this.r + foodItem.r) {
+            let sum = PI * this.r * this.r + PI * foodItem.r * foodItem.r
+            this.r = sqrt(this.r ** 2 + Math.exp(-this.r + 64) * foodItem.r**2)
+            return true
+        }
+        return false;
+    }
+    //TODO: add constrain to server
+    this.constrain = function () {
+        this.pos.x = constrain(this.pos.x, -width, width)
+        this.pos.y = constrain(this.pos.y, -height, height)
+    }
+
+    let red = randomHex()
+    let green = randomHex()
+    let blue = randomHex()
+    this.show = function () {
+        fill(red, green, blue)
+        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    }
+}
+
+function randomHex() {
+    return Math.floor(Math.random() * 256)
+}
+
+joinButton.addEventListener('click', () => {
+    const modal = document.querySelector('#join-game-modal')
+    modal.style.display = 'none'
+    playerJoinEvent()
+    currentGame.startGame = true;
+})
+
+const playerJoinEvent = () => {
+    socket.emit("PlayerJoinRequest", 0 , (id, px, py) => {
+        console.log('PlayerJoinRequest response', id, px, py);
+        currentGame.playerID = id;
+        ply = currentGame.player();
+        ply.model.pos.x = px;
+        ply.model.pos.y = py;
+    });
+}
 
 socket.on('PlayerJoined', (pid, blob) => {
     currentGame.players[pid] = {
@@ -126,22 +189,6 @@ socket.on("GameUpdate", (contents) => {
     socket.emit("PlayerUpdate", currentGame.player().model.pos.x, currentGame.player().model.pos.y, currentGame.player().model.velocity.x, currentGame.player().model.velocity.y);
 });
 
-// Setup plan with server/client code
-// Food will be instantiated by the server - an array 
-// will be sent over when the canvas is first drawn - foodEaten events will also be sent
-// by the server when a blob feasts
-const playerJoinEvent = () => {
-    socket.emit("PlayerJoinRequest", 0 , (id, px, py) => {
-        console.log('PlayerJoinRequest response', id, px, py);
-        currentGame.playerID = id;
-        ply = currentGame.player();
-        ply.model.pos.x = px;
-        ply.model.pos.y = py;
-    });
-}
-    
-
-
 socket.on('FoodCreated', (id, x, y) => {
     currentGame.food[id] = new Blob(x, y, 15);
     currentGame.food[id].id = id;
@@ -150,57 +197,4 @@ socket.on('FoodCreated', (id, x, y) => {
 socket.on('FoodEaten', (id) => {
     currentGame.food.splice(id, 1);
     console.log(`food ${id} eaten`);
-})
-
-
-
-function Blob(x, y, r) {
-    this.pos = createVector(x, y);
-    this.r = r;
-    this.velocity = createVector(0,0)
-
-    this.control = function() {
-        let newVelocity = createVector(mouseX-width/2, mouseY-height/2);
-        newVelocity.setMag(3);
-        this.velocity.lerp(newVelocity, .3);
-    }
-
-    this.update = function() {
-        this.pos.add(this.velocity);
-    }
-
-    this.eats = function (foodItem) {
-        let d = p5.Vector.dist(this.pos, foodItem.pos)
-        if (d < this.r + foodItem.r) {
-            let sum = PI * this.r * this.r + PI * foodItem.r * foodItem.r
-            this.r = sqrt(this.r ** 2 + Math.exp(-this.r + 64) * foodItem.r**2)
-            return true
-        }
-        return false;
-    }
-    
-    //add constrain to server
-    this.constrain = function () {
-        this.pos.x = constrain(this.pos.x, -width, width)
-        this.pos.y = constrain(this.pos.y, -height, height)
-    }
-
-    let red = randomHex()
-    let green = randomHex()
-    let blue = randomHex()
-    this.show = function () {
-        fill(red, green, blue)
-        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
-    }
-}
-
-function randomHex() {
-    return Math.floor(Math.random() * 256)
-}
-
-joinButton.addEventListener('click', () => {
-    const modal = document.querySelector('#join-game-modal')
-    modal.style.display = 'none'
-    playerJoinEvent()
-    currentGame.startGame = true;
 })
