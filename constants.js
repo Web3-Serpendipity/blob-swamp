0f39d6eb10bf0f9bc6c63e4d57b60d16eaa3a969
Peter Myers <peter@pcmyers.com>
2022-03-23 19:37:41 -0400

    Remove constants, and add constants.js to gitignore

--- constants.js
+++
@@ -1,5 +0,0 @@
-module.exports.RPC_URL = "https://rpc-mumbai.maticvigil.com";
-module.exports.WALLET_SEED = "table pony rapid kiss share sign imitate alpha artist brush fiction old";
-//module.exports.CONTRACT_ADDRESS = "0x2c12d4F3d407D1a3A11Fb71D2E836f290C85bdBa"; // rinkeby
-module.exports.CONTRACT_ADDRESS = "0xB1276cF187006C13f1E8932b6724c5Fe8c5f6F6F";
-module.exports.WALLET_ADDRESS = "0x98399fe09475dc1ae21136aa015497fe3db36c30";
