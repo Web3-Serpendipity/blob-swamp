1105d55e954030205431d552301888b8b8807957
Peter Myers <peter@pcmyers.com>
2022-03-24 01:39:51 -0400

    Add test for elements and sparse arrays

--- test/test.js
+++ test/test.js
@@ -1,8 +1,24 @@
 var insert = require('../insert')
 
-describe('basic tests', function () {
+describe('testing insert array functionality', function () {
     it('should not be undefined', function () {
         if (insert.insert([0,1], 3) !== undefined){
+            return true;
+        } else {
+            return false;
+        }
+    })
+    
+    it('should have three elements', function () {
+        if (insert.insert([0,1], 3).lenghth == 3){
+            return true;
+        } else {
+            return false;
+        }
+    })
+
+    it('should fill in holes', function () {
+        if (insert.insert([0,1,undefined, 4], 3) == [0,1,2,4]){
             return true;
         } else {
             return false;
