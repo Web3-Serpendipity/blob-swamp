64df5c935d8f7136c1bf22823cd38450d7b8609d
Peter Myers <peter@pcmyers.com>
2022-03-25 15:39:27 -0400

    Import module correctly

--- test/player_test.js
+++ test/player_test.js
@@ -1,8 +1,15 @@
-var player = require('../player');
-
+const { Player } = require('../player');
+var assert = require('assert')
 describe('A player', function (){
     it('can be created', function () {
-        if (player == undefined ) {
+        if (Player() == undefined ) {
+            return false; // test fails
+        } else {
+            return true;
+        }
+    });
+    it('will have correct attribute types', function () {
+        if (new Player().id == undefined ) {
             return false; // test fails
         } else {
             return true;
