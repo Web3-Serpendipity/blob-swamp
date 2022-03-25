7879fa8971521562a2ec0c87666e6ef40a294b2b
josiahwebb <jj.webb520@gmail.com>
2022-03-25 14:24:31 -0600

    adds assert tests for Player, comments out test.js

--- test/player_test.js
+++ test/player_test.js
@@ -2,9 +2,18 @@ const { Player } = require('../player');
 var assert = require('assert')
 describe('A player', function (){
     it('can be created', function () {
-        return (Player() == undefined );
-    });
+        assert.notEqual(new Player(), undefined);
+    })
     it('will have correct attribute types', function () {
-        return (new Player().id == undefined);
+        assert.equal(new Player().id, -1);
+    })
+    it('will have correct number of attributes', function () {
+        assert.equal(Object.keys(new Player()).length, 8);
+    })
+    it('will have a nonce', () => {
+        assert.notEqual(new Player().nonce, undefined)
+    })
+    it('will only have number for a nonce', () => {
+        assert.equal((typeof new Player().nonce), 'number')
     })
 })
--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
-var insert = require('../insert')
+// var insert = require('../insert')
 
-describe('testing insert array functionality', function () {
-    it('should not be undefined', function () {
-        return (insert.insert([0,1], 3) !== undefined);
-    })
+// describe('testing insert array functionality', function () {
+//     it('should not be undefined', function () {
+//         return (insert.insert([0,1], 3) !== undefined);
+//     })
     
-    it('should have three elements', function () {
-        return (insert.insert([0,1], 3).lenghth == 3);
-    })
+//     it('should have three elements', function () {
+//         return (insert.insert([0,1], 3).lenghth == 3);
+//     })
 
-    it('should fill in holes', function () {
-        return (insert.insert([0,1,undefined, 4], 3) == [0,1,2,4]);
-    })
-})
+//     it('should fill in holes', function () {
+//         return (insert.insert([0,1,undefined, 4], 3) == [0,1,2,4]);
+//     })
+// })
