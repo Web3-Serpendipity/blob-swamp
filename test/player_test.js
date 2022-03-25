d4072269d605754573cb73382b2d0559c8d317f4
Peter Myers <peter@pcmyers.com>
2022-03-25 15:44:54 -0400

    Clean tests

--- test/player_test.js
+++ test/player_test.js
@@ -2,17 +2,9 @@ const { Player } = require('../player');
 var assert = require('assert')
 describe('A player', function (){
     it('can be created', function () {
-        if (Player() == undefined ) {
-            return false; // test fails
-        } else {
-            return true;
-        }
+        return (Player() == undefined );
     });
     it('will have correct attribute types', function () {
-        if (new Player().id == undefined ) {
-            return false; // test fails
-        } else {
-            return true;
-        }
+        return (new Player().id == undefined);
     })
 })
--- test/test.js
+++ test/test.js
@@ -2,26 +2,14 @@ var insert = require('../insert')
 
 describe('testing insert array functionality', function () {
     it('should not be undefined', function () {
-        if (insert.insert([0,1], 3) !== undefined){
-            return true;
-        } else {
-            return false;
-        }
+        return (insert.insert([0,1], 3) !== undefined);
     })
     
     it('should have three elements', function () {
-        if (insert.insert([0,1], 3).lenghth == 3){
-            return true;
-        } else {
-            return false;
-        }
+        return (insert.insert([0,1], 3).lenghth == 3);
     })
 
     it('should fill in holes', function () {
-        if (insert.insert([0,1,undefined, 4], 3) == [0,1,2,4]){
-            return true;
-        } else {
-            return false;
-        }
+        return (insert.insert([0,1,undefined, 4], 3) == [0,1,2,4]);
     })
 })
