caee907a3162cfbebd5c6409969f4323fbc0670a
Peter Myers <peter@pcmyers.com>
2022-03-25 15:35:35 -0400

    Create player test file

---
+++ test/player_test.js
@@ -0,0 +1,11 @@
+var player = require('../player');
+
+describe('A player', function (){
+    it('can be created', function () {
+        if (player == undefined ) {
+            return false; // test fails
+        } else {
+            return true;
+        }
+    })
+})
