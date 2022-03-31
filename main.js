c85bdb15e9b70aaec5a3c0ca3be4e40b797c93bc
pcm <99992004+sassafrasass@users.noreply.github.com>
2022-03-31 14:30:38 -0400

    Revert "death screen"

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const isMetaMaskInstalled = () => ethereum.isMetaMaskInstalled
 const dlBtn = document.getElementById('dl-btn')
 const socket = io(window.location.href);
 const showAccount = document.querySelector('#show-account')
-const returnBtn = document.querySelector('#return-btn')
-const deathModal = document.querySelector('.u-ded-modal')
-const joinModal = document.querySelector('#join-game-modal')
-
 
 let signerNonce;
 let isAuthenticated = false; //TODO: must be false in final, change to true to skip authentication
@@ -157,7 +153,8 @@ function randomHex() {
 }
 
 joinButton.addEventListener('click', () => {
-    joinModal.style.display = 'none'
+    const modal = document.querySelector('#join-game-modal')
+    modal.style.display = 'none'
     playerJoinEvent()
     currentGame.startGame = true;
 })
@@ -182,7 +179,8 @@ socket.on('PlayerJoined', (pid, blob) => {
 
 socket.on('PlayerLeft', (pid) => {
     if (pid == currentGame.playerID) {
-        deathModal.style.display = 'flex'
+        const modal = document.querySelector('#join-game-modal')
+        modal.style.display = 'block'
         currentGame.startGame = false;
     } // TODO: handle this.
     delete currentGame.players[pid];
@@ -212,9 +210,4 @@ socket.on('FoodCreated', (id, x, y) => {
 socket.on('FoodEaten', (id) => {
     currentGame.food.splice(id, 1);
     console.log(`food ${id} eaten`);
-})
-
-returnBtn.addEventListener('click', () => {
-    joinModal.style.display = 'block'
-    deathModal.style.display = 'none'
 })
